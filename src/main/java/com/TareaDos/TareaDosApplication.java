9249583dd7b91ba7e1b73e22cdaf83a512f00bc1
vquiros1996 <viviqm127@hotmail.com>
2023-10-03 17:12:43 -0600

    Repositorio inicializado de tarea dos

---
+++ src/main/java/com/TareaDos/TareaDosApplication.java
@@ -0,0 +1,13 @@
+package com.TareaDos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaDosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaDosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TareaDos/TareaDosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TareaDos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaDosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
